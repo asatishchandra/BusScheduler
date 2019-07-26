2a19785d8b321e7004ff075f2f1b04c63573c7ca
Satish Ayyagari <sayyagari@fcci.us.com>
2019-07-26 11:49:25 -0700

    Adding BusesController and DTO to the API project.

---
+++ BusScheduleApi/Controllers/BusesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusScheduleApi.DTO;
+using BusScheduleSevices.Models;
+using BusScheduleSevices.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusScheduleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusesController : ControllerBase
+    {
+        public BusScheduleService busScheduleService;
+
+        public BusesController()
+        {
+            busScheduleService = new BusScheduleService();
+        }
+        // GET api/busues
+        [HttpGet]
+        public ActionResult<IEnumerable<BusStopRouteDto>> Get()
+        {
+            List<BusStopRouteDto> dto = new List<BusStopRouteDto>();
+            List<BusStop> busStopsAndRoutes = busScheduleService.GetAllStopRouteData();
+
+            foreach (var stop in busStopsAndRoutes)
+            {
+                dto.Add(new BusStopRouteDto { BusStop = stop.StopName, BusRoutes = stop.StopSchedule });
+            }
+            return dto;
+        }
+
+        // GET api/buses/3:01
+        [HttpGet("{time}")]
+        public ActionResult<IEnumerable<BusStopRouteDto>> Get(string time)
+        {
+            List<BusStopRouteDto> dto = new List<BusStopRouteDto>();
+            List<BusStop> busStopsAndRoutes = busScheduleService.GetNextTwoBusArrivalDataByTime(time);
+
+            foreach (var stop in busStopsAndRoutes)
+            {
+                dto.Add(new BusStopRouteDto { BusStop = stop.StopName, BusRoutes = stop.StopSchedule });
+            }
+            return dto;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BusScheduleApi/DTO/BusStopRouteDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusScheduleApi.DTO
+{
+    public class BusStopRouteDto
+    {
+        public string BusStop { get; set; }
+        public Dictionary<string, List<string>> BusRoutes { get; set; }
+    }
+}
